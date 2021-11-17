00fe8cee66df43d2de8b20816e3328cabf2903af
Sorrecer <Anagakenny24@gmail.com>
2021-11-17 22:38:26 +0700

    index

--- user/index.php
+++ user/index.php
@@ -10,6 +10,11 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <style>
     .column {
+      float: right;
+      width: 50%;
+    }
+
+    .column1 {
       float: left;
       width: 50%;
     }
@@ -20,23 +25,43 @@
       display: table;
       clear: both;
     }
+
+    #rcorners1 {
+      border-radius: 25px;
+      background: rgba(245, 238, 255, 0.5);
+      padding: 20px;
+      width: 400px;
+      height: 600px;
+    }
+
+    .sb {
+      position: relative;
+      left: 20px;
+    }
   </style>
 
 </head>
 
 <body>
+  <div class="column1">
+    <div class="text-white d-block justify-content-center my-4" style="font-size:30px">
+      <div>
+        <img class="sb" src="../sb1.png" width="250" height="60" class="d--block align-top" alt="">
+      </div>
+      <div class="container d-grid my-1  " style="font-size: 70px; left :auto;"><b> Mengenal Budaya <br>Indonesia dengan<br> Menyenangkan</b>
+      </div>
+      <div>
+        <img src="../peta1.png" width="500" height="200" class="d--block align-top" alt="">
+      </div>
+    </div>
+  </div>
 
-  <div class="text-white d-flex justify-content-center my-4">
-    <h1>ini login</h1>
-  </div>
-  </style>
   <div class="column">
-    <div class="text-white d-flex justify-content-center my-4" style="font-size:30px">ya nggak tau</div>
-  </div>
+    <div class="text-white d-flex justify-content-center my-4" style="font-size: 40px">
+      <div id="rcorners1">
 
-  <div class="column">
-    <div class="text-white d-flex justify-content-center my-4" style="font-size:30px">tanya kok tanya saya</div>
-  </div>
+      </div>
+    </div>
   </div>
 
 </body>
