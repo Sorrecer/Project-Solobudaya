563fa582ae40c7f317917a113c07bce646127a9a
Sorrecer <Anagakenny24@gmail.com>
2021-11-17 20:22:34 +0700

    commit header

--- lib/header.php
+++ lib/header.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
         body {
-            background-image: url('../bg1.jpg');
+            background-image: url('../bg1.png');
             background-repeat: no-repeat;
             background-attachment: fixed;
             background-size: 100% 130%;
@@ -19,22 +19,8 @@
 </head>
 
 <body>
-    <?php
-    session_start();
-    $path = $_SERVER['REQUEST_URI'];
-    if(strpos($path, "/admin/") !== false){
-        include("../lib/admin_login_redirect.php");
-    }
-    elseif(strpos($path, "/user/") !== false){
-        include("../lib/login_redirect.php");
-    }
-    if (isset($_SESSION['username'])) {
-        if ($_SESSION['kategori'] == 'petugas') {
-            include("../lib/header-admin.php");
-        } elseif ($_SESSION['kategori'] == 'anggota') {
-            include("../lib/header-user.php");
-        }
-    } else {
-        include("../lib/header-visitor.php");
-    }
-    ?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-5">
+        <div class="container container-fluid">
+            <a href="../user/index.php" class="navbar-brand">Kenny Lib</a>
+        </div>
+    </nav>
