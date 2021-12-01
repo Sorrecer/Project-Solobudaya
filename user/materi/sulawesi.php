320d1d2b41eddd39274b748ec0275e7e71b11e80
Sorrecer <Anagakenny24@gmail.com>
2021-12-01 20:16:36 +0700

    .

---
+++ user/materi/sulawesi.php
@@ -0,0 +1,21 @@
+<?php include("../../lib/header-pilih-provinsi.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pilih Provinsi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+    </style>
+</head>
+<div class="container justify-content-center" style="text-align:center;font-weight:bolder;font-size: 50px; color:indigo;padding-top:50px">
+    Pilih Provinsi
+    <div>
+        <img id="sulawesi" src="../../images/sulawesi.png" width="50%">
+    </div>
+</div>
+
+<body>
+    <?php include("../../lib/footer.php"); ?>
