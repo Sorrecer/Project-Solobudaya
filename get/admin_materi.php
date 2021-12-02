ed4a0649804a263adc752ed85f11342ece73886e
TheQuadruplity <thev.vandicco@gmail.com>
2021-12-02 13:22:44 +0700

    edit materi dikirim get

--- get/admin_materi.php
+++ get/admin_materi.php
@@ -8,7 +8,7 @@ while($row = $res->fetch_object()){
     <td>$row->judul_materi</td>
     <td>Tarian Adat</td>
     <td><div class='d-grid gap-1'>
-    <a href='edit_materi.php?id=$row->id_materi'><button class='btn btn-primary btn-sm'>Edit</button></a>
+    <a href='edit_materi.php?id=$row->id_materi?id_provinsi=$id_prov'><button class='btn btn-primary btn-sm'>Edit</button></a>
     <a href=''><button class='btn btn-danger btn-sm' onclick = 'return confirm (\'Apakah anda yakin ingin menghapus?\')'>Delete</button></a>
     <a href='lihat_materi.php?id_materi=$row->id_materi'><button class='btn btn-primary btn-sm'>Lihat</button></a>
     </div></td>
