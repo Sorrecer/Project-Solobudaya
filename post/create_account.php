8551c50530b537d23caf776a9bde9d793bd6aef0
TheQuadruplity <thev.vandicco@gmail.com>
2021-12-07 23:25:15 +0700

    create account fixed

--- post/create_account.php
+++ post/create_account.php
@@ -6,7 +6,7 @@ $now = date('Y-m-d');
 
 make_query("INSERT INTO akun SET username='$username', password='$password'");
 $id = $GLOBALS['db']->insert_id;
-make_query("INSERT INTO user SET id_user=$id, tgl_gabung='$now' foto_profil=2");
+make_query("INSERT INTO user SET id_user=$id, tgl_gabung='$now', foto_profil=2");
 
 header('Location: ../user/index.php');
 
