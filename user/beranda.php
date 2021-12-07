a8a9ba1b367ec8d9ad0dcf2c89c66cdf7a2f9948
Sorrecer <Anagakenny24@gmail.com>
2021-12-07 20:58:53 +0700

    a

--- user/beranda.php
+++ user/beranda.php
@@ -8,6 +8,102 @@
         background-size: 100% 130%;
     }
 
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    section {
+        position: relative;
+        width: 100%;
+        height: 100vh;
+        background: #000;
+        overflow: hidden;
+    }
+
+    section.wave {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 198px;
+        background: url('../images/wave.png');
+        background-size: 1600px 198px;
+    }
+
+    section .wave.wave1 {
+        animation: animate-wave-1 10s linear infinite;
+        z-index: 1000;
+        opacity: 1;
+        animation-delay: 0s;
+        bottom: 0;
+    }
+
+    section .wave.wave2 {
+        animation: animate-wave-2 5s linear infinite;
+        z-index: 999;
+        opacity: 0.5;
+        animation-delay: -5s;
+        bottom: 10;
+    }
+
+    section .wave.wave3 {
+        animation: animate-wave-3 10s linear infinite;
+        z-index: 998;
+        opacity: 0.2;
+        animation-delay: -2s;
+        bottom: 15;
+    }
+
+    section .wave.wave4 {
+        animation: animate-wave-4 2s linear infinite;
+        z-index: 997;
+        opacity: 0.7;
+        animation-delay: -5s;
+        bottom: 20;
+    }
+
+    @keyframes animate-wave-1 {
+        0% {
+            background-position-x: 0;
+        }
+
+        100% {
+            background-position-x: 1600px;
+        }
+    }
+
+    @keyframes animate-wave-2 {
+        0% {
+            background-position-x: 0;
+        }
+
+        100% {
+            background-position-x: -1600px;
+        }
+    }
+
+    @keyframes animate-wave-3 {
+        0% {
+            background-position-x: 0;
+        }
+
+        100% {
+            background-position-x: 1600px;
+        }
+    }
+
+    @keyframes animate-wave-4 {
+        0% {
+            background-position-x: 0;
+        }
+
+        100% {
+            background-position-x: -1600px;
+        }
+    }
+
     .column {
         width: 50%;
     }
@@ -181,5 +277,10 @@
                 </div>
             </div>
         </div>
-
+        <section>
+            <div class="wave wave1"></div>
+            <div class="wave wave2"></div>
+            <div class="wave wave3"></div>
+            <div class="wave wave4"></div>
+        </section>
         <?php include("../lib/footer.php"); ?>
