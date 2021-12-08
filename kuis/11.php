01ca2d8102c21a95794544be2e0fa894088de5b1
DavidpPakpahan <davidpakpahan25@gmail.com>
2021-12-08 22:20:15 +0700

    kuis pulau sumatera

---
+++ kuis/11.php
@@ -0,0 +1,22 @@
+<div class="column1" style="font-size: 25px;font-weight: bold; align-items: center;justify-content: center;">
+    <h1>Senjata</h1>
+
+
+      <p>Bentuk dari senjata adat ini juga mirip 
+         dengan keris, namun mempunyai ukuran yang lebih pendek dan lebih kecil daripada senjata 
+         keris yang digunakan untuk perang pada umumnya. Nama senjata ini adalah?
+      </p>
+
+    </div>
+
+    <div class="column2" style="display:flex;justify-content: center;align-items: center;">
+        <img src="../images/materi/Senjata-Tradisional-siwar-Bukalapak.jpg" width="500px" height="relative">
+    </div>
+
+<?php 
+    // jawaban benar
+    $ans[1] = ['Siwar Panjang'];
+
+    // jawaban salah
+    $ans[0] = ['Piso Gaja Dombak', 'Badik Tumbuk Lada', 'Pedang Jenawi'];
+?>
---
+++ kuis/8.php
@@ -0,0 +1,21 @@
+<div class="column1" style="font-size: 25px;font-weight: bold; align-items: center;justify-content: center;">
+    <h1>Senjata</h1>
+
+
+      <p>Hanya para raja – raja yang mempunyai senjata ini. Kemudian diwariskan secara turun 
+         temurun hingga kini. Nama senjata ini adalah?
+      </p>
+
+    </div>
+
+    <div class="column2" style="display:flex;justify-content: center;align-items: center;">
+        <img src="../images/materi/Senjata-Tradisional-piso-gaja-dompak-sumut.jpg" width="500px" height="relative">
+    </div>
+
+<?php 
+    // jawaban benar
+    $ans[1] = ['Piso Gaja Dombak'];
+
+    // jawaban salah
+    $ans[0] = ['Pedang Jenawi', 'Siwar Panjang', 'Badik Tumbuk Lada'];
+?>
