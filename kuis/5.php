e1674e25f1ea65532ca700b0b15ccea5d6f02598
DavidpPakpahan <davidpakpahan25@gmail.com>
2021-12-08 22:19:52 +0700

    kuis pulau jawa

---
+++ kuis/5.php
@@ -0,0 +1,17 @@
+<div class="column1" style="font-size: 25px;font-weight: bold; align-items: center;justify-content: center;">
+    <h1>Senjata</h1>
+
+
+    <p>Yogyakarta mempunyai kebudayaan keraton yang masih bertahan hingga kini. Apa nama senjata 
+khas keraton Yogyakarta berikut?</p>
+
+    </div>
+
+    <div class="column2" style="display:flex;justify-content: center;align-items: center;">
+        <img src="../images/materi/Senjata-Tradisional-keris-yogyakarta-Sobat-Jogja-630x380.jpg" width="500px" height="relative">
+    </div>
+
+<?php 
+    $ans[1] = ['Keris'];
+    $ans[0] = ['Golok', 'Plinteng', 'Celurit'];
+?>
